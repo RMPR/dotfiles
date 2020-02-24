1999756bede138c899a8e00d3340c90d5896852e
RMPR <akoudanilo@gmail.com>
2020-02-24 19:36:21 +0100

    Initial commit

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,127 @@
+/home/rufus/Documents/WileyFox Spark 2019/Music/Age of Empires III Soundtrack - Main Theme.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Agni Kai - Avatar_ The Last Airbender Soundtrack.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Asphalt 8/Be Electric The Qemists【Asphalt 8-Airborne OST】 - YouTube.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Asphalt 8/Burning - DJ Gontran【Asphalt 8-Airborne OST】 1.3.0 - YouTube.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Asphalt 8/Far Too Loud - Lightbringer - YouTube.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Asphalt 8/Martin Garrix - Animals (Official Video) - YouTube.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Asphalt 8/MONSTA - Holdin' On (Skrillex & Nero Remix) - YouTube.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Asphalt 8/Mutemath - Blood Pressure [Official Music Video] - YouTube.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Asphalt 8/Nero - Etude - YouTube.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Attack on Titan S3 OST - 0Sk (Extended).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Attack on Titan Season 2 opening Full『Linked Horizon - Shinzou wo Sasageyo!』.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Avatar_The_Last_Airbender_Theme_Song_(HQ)__Episode_Opening_Credits__Nick_Animation.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Chopin - Spring Waltz.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/CKY- Testing.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Daft Punk - Fragile Original Version Tron Legacy Soundtrack (unreleased) 2015.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Daft Punk - Make Love (Official audio).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Daft Punk - The Son of Flynn (TRON LEGACY).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Daft Punk - Tron Legacy End of Line (Alexel Remix).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Divine Medium - Avatar The Last Airbender Soundtrack (mp3goo.com).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Duel Of The Fates from Star Wars Episode 1 The Phantom Menace (Official Audio).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Elijah Bossenbroek - I give up.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Ellie Goulding - Love Me Like You Do Cover (CelloPiano) - Brooklyn Duo.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Final Fantasy 3 - Terra's Theme.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Full Age of Empires III OST.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Full opening naruto Blue Bird [Lyrics].mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Game of Thrones - Main Theme (Extended) HD.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Game of Thrones - Main Titles - Ramin Djawadi (Season 7 Soundtrack) [official].mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Game of Thrones Season 6 OST - Light of the Seven (EP 10 Trial scene).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Gramatik - Tearin It Up.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/How to get away with murder original score (Photek Music).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/How to Get Away with Murder theme.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/If Julia Michaels Issues were a Christian song by Beckah Shae LYRICS.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Jacob Banks - Chainsmoking.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Jacob Banks - Unknown (To You) Official Music Video.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/La casa de papel - Bella Ciao.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Legend of Korra Main Theme - Legend of Korra Sound Track.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Legend of Korra The Game Soundtrack (Game Specific Music) HD Soundtrack.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Lindsey Stirling/Crystallize - Lindsey Stirling (Dubstep Violin Original Song) - YouTube.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Lindsey Stirling/First Light - Lindsey Stirling - YouTube.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Lindsey Stirling/Forgotten City from RiME - Lindsey Stirling - YouTube.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Lindsey Stirling/Lindsey Stirling - Dance of the Sugar Plum Fairy - YouTube.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Lindsey Stirling/Master of Tides - Lindsey Stirling - YouTube.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Lindsey Stirling/River Flows In You- Lindsey Stirling - YouTube.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Lindsey Stirling/Roundtable Rival - Lindsey Stirling.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Lindsey Stirling/Shadows - Lindsey Stirling (Original Song).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Lindsey Stirling/Spontaneous Me -Lindsey Stirling (original song) - YouTube.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Lindsey Stirling/Zelda Medley- Lindsey Stirling.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Lorde - Royals (US Version).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Lorde - Yellow Flicker Beat (Music Video).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Main Opening Theme - Avatar The Last Airbender Soundtrack.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Matthew Morgan - Sun Through the Clouds.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Michael Nyman - One Moment.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/mission impossible theme song.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Naruto Shippuden - Girei Pains Theme (English Lyrics).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Orbital - Halcyon On and On.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Pink Panther Theme Song.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Pirates of the Caribbean (Auckland Symphony Orchestra) 1080p_big.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Pirates of the Caribbean (Auckland Symphony Orchestra) 1080p.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Pirates Of The Caribbean Theme Song.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - Ho Ho Ho (Lyrics).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - Im Still Here [Lyric Video] [NEW SONG 2018] - evproductions_.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - Suitcase (Music Video) ft. Maddie Ziegler.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - Summer Rain (Official Audio).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - Angel By The Wings (Lyrics).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - Everyday Is Christmas.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - House On Fire (Audio).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - Never Give Up (from the Lion Soundtrack) [Lyric Video].mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - Santas Coming For Us (Lyrics).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - Snowman.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - Soon Well Be Found (Letterman).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - Soon Well Be Found.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Greatest.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Star Wars- The Imperial March (Darth Vaders Theme).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Stromae - carmen.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Super Smash Bros Complete Soundtrack OST Nintendo 64.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/The big bang theory ending theme.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/The_Big_Bang_Theory_Intro_Full_Version.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/The_Big_Bang_Theory_-_Opening_-_HD.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/The Legend of Korra - OST (the game) - Republic City.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/The Legend of Korra Theme Song (HQ)  Episode Opening Credits  Nick Animation.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Tokyo Ghoul - Unravel [Lyrics].mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Trailer Music Warcraft The Beginning (Theme Music) - Soundtrack Warcraft The Beginning.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/White Collar Music.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/White Collar Theme Song...Best 1.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Avatar_The Last Airbender - The Peace Soundtrack (Mp3FB.com).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/naruto shippuden opening 9 full HD sasuke vs danzo.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Adele - Rolling in the Deep.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Erza Muqoli - La Bohème (Studio Remastered).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Alessia Cara   Here Beckah Shae's Way.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Beckah shae Attention (cover) 8 bit.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Titanium (feat. Sia).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Beckah Shae - Stay (Lyric Video).mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/If Taylor Swift's Look What You Made Me Do were a Christian song by Beckah Shae LYRICS.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Imagine Dragons - Believer.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/klaus_badelt-pirates_of_the_caribbean_the_curse_of_the_black_pearl.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/fat-joe-feat-lil-wayne-make-it-rain-instrumental.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Pentatonix/04 - Aha!.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Pentatonix/03 - Carol of the Bells.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Pentatonix/01 - Radioactive.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Pentatonix/01 - La La Latch.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Pentatonix/19 - Royals.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Pentatonix/07. Dance of the Sugar Plum Fairy.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Pharrell Williams - Happy.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/sia-waving-goodbye-lyrics.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - I Go To Sleep .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - Clap Your Hands .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - Big Girls Cry .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - Elastic Heart .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - Fire Meet Gasoline .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - Chandelier (Piano Version) .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - Chandelier .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - Elastic Heart (Piano Version) .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - Alive .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - Bird Set Free .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - Cheap Thrills .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - One Million Bullets .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - California Dreamin' .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - Breathe Me .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - Reaper .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - Move Your Body .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - Unstoppable .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - Footprints .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - Sweet Design .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/Sia - The Best Songs (2016) [MP3~320Kbps]~[Hunter] [FRG]/Sia - The Best Songs/Sia - Broken Glass .mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/LSD - Thunderclouds (Official Video) ft. Sia, Diplo, Labrinth.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/If Ed Sheeran's  Shape of You  were a Christian song by Beckah Shae.mp3
+/home/rufus/Documents/WileyFox Spark 2019/Music/If Camila Cabello's _Havana_ were a Christian song by Beckah Shae (Lyrics).mp3
---
+++ .profile
@@ -0,0 +1,5 @@
+export EDITOR=vim
+export GDK_BACKEND=wayland
+export CLUTTER_BACKEND=wayland
+PATH="$HOME/bin:$PATH"
+export _JAVA_AWT_WM_NONREPARENTING=1
