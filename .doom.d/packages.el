2a260903d5c66b35e4fa263782db5c675ece19f6
RMPR <akoudanilo@gmail.com>
2020-10-10 20:15:41 +0100

    Install reveal.js to Doom Emacs

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -16,7 +16,7 @@
 
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
-;(package! some-package)
+(package! ox-reveal)
 
 ;; To install a package directly from a particular repo, you'll need to specify
 ;; a `:recipe'. You'll find documentation on what `:recipe' accepts here:
