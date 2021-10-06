4d49b0760b5adf39b609214628afc70089dddde5
Paul Mairo <herrmpr@gmail.com>
2021-10-06 07:55:28 +0100

    Make the damn LSP client work
    Configure root detection for LSP and fix a minor depreaction warning in
    alacritty.

--- .config/i3/scripts/kde-i3.sh
+++ .config/i3/scripts/kde-i3.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 export KDEWM=/usr/bin/i3
+/usr/bin/i3
--- .config/nvim/lua/lsp_config.lua
+++ .config/nvim/lua/lsp_config.lua
@@ -1,7 +1,35 @@
--- vim.cmd [[ packadd completion-nvim ]]
+local configs = require 'lspconfig/configs'
+local util = require 'lspconfig/util'
+local nvim_lsp = require('lspconfig')
+local completion = require 'completion'
 
-local lsp = require("lspconfig")
-local completion = require("completion")
+-- Python Configuration
+configs.pyls = {
+    default_config = {
+        cmd = { 'pyls' },
+        filetypes = { 'python' },
+        root_dir = function(fname)
+            local root_files = {
+                'pyproject.toml',
+                'setup.py',
+                'setup.cfg',
+                'requirements.txt',
+                'Pipfile',
+            }
+            return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
+        end,
+    },
+    docs = {
+        description = [[
+        https://github.com/python-lsp/python-lsp-server
+        A Python 3.6+ implementation of the Language Server Protocol.
+        The language server can be installed via `pipx install 'python-lsp-server[all]'`.
+        Further instructions can be found in the [project's README](https://github.com/python-lsp/python-lsp-server).
+        Note: This is a community fork of `pyls`.
+        ]],
+    },
+}
+
 
 -- Create an helper function to bind new mappings
 local mapper = function(mode, key, result)
@@ -28,9 +56,9 @@ local custom_attach = function()
 end
 
 -- Just add the server name to the list of servers
-local servers = {"pyls", "rust_analyzer", "gopls"}
+local servers = {'pyls', 'rust_analyzer', 'gopls'}
   for _, ls in ipairs(servers) do
-   lsp[ls].setup {
+   nvim_lsp[ls].setup {
      on_attach = custom_attach,
    }
 end
