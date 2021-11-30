4162a851ca04707cb43bc3176d0f185687243a2c
Paul Mairo <herrmpr@gmail.com>
2021-11-30 21:08:35 +0100

    Synchronize after updating Thinkpad Yoga to f35

--- .config/nvim/lua/lsp_config.lua
+++ .config/nvim/lua/lsp_config.lua
@@ -2,35 +2,8 @@ local configs = require 'lspconfig/configs'
 local util = require 'lspconfig/util'
 local nvim_lsp = require('lspconfig')
 local completion = require 'completion'
-
-
--- Python Configuration
-configs.pyls = {
-    default_config = {
-        cmd = { 'pyls' },
-        filetypes = { 'python' },
-        root_dir = function(fname)
-            local root_files = {
-                'pyproject.toml',
-                'setup.py',
-                'setup.cfg',
-                'requirements.txt',
-                'Pipfile',
-            }
-            return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
-        end,
-    },
-    docs = {
-        description = [[
-        https://github.com/python-lsp/python-lsp-server
-        A Python 3.6+ implementation of the Language Server Protocol.
-        The language server can be installed via `pipx install 'python-lsp-server[all]'`.
-        Further instructions can be found in the [project's README](https://github.com/python-lsp/python-lsp-server).
-        Note: This is a community fork of `pyls`.
-        ]],
-    },
-}
-
+require 'rust-lsp'
+require 'python-lsp'
 
 -- Create an helper function to bind new mappings
 local mapper = function(mode, key, result)
