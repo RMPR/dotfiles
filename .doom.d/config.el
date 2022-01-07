be80ce364ce49abb9124d8612e2c8d7f42e02466
Rufus Mairo <herrmpr@gmail.com>
2022-01-07 14:23:39 +0100

    Install org-roam-ui

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -61,6 +61,21 @@
 
 (setq org-roam-v2-ack t)
 
+(use-package! websocket
+    :after org-roam)
+
+(use-package! org-roam-ui
+    :after org-roam ;; or :after org
+;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;;         a hookable mode anymore, you're advised to pick something yourself
+;;         if you don't care about startup time, use
+;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
+
 (setq org-journal-date-prefix "#+TITLE: "
       org-journal-time-prefix "* "
       org-journal-date-format "%a, %Y-%m-%d"
--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -48,3 +48,6 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+;; Roam UI stuff
+(unpin! org-roam)
+(package! org-roam-ui)
