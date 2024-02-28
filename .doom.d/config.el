7126f052f713b4d18b4c9d8785e063c4f215145b
Paul Mairo <herrmpr@gmail.com>
2024-02-28 21:01:01 +0100

    Daily Save

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -29,7 +29,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/todos/todos")
+(setq org-directory "~/todos/")
 (setq org-agenda-files '("~/todos/todos/todos.org"))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
